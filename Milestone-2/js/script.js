28317d4fbcbf336c5762ab363205d6e996ebf3d2
Yomakota <teoperitore@outlook.com>
2022-06-29 12:40:30 +0200

    milestone-2

--- Milestone-2/js/script.js
+++ Milestone-2/js/script.js
@@ -1,11 +1,13 @@
-// Milestone 1
-// Replica della grafica con la possibilità di avere messaggi scritti dall’utente(verdi) e dall’interlocutore(bianco) assegnando due classi CSS diverse
-// Visualizzazione dinamica della lista contatti: tramite la direttiva v -for, visualizzare nome e immagine di ogni contatto
+// Milestone 2
+// Visualizzazione dinamica dei messaggi: tramite la direttiva v -for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// Click sul contatto mostra la conversazione del contatto cliccato
 
-var app = new Vue (
+var app = new Vue(
     {
         el: '#root',
         data: {
+
+            currentActiveElement: 0,
 
             contacts: [
                 {
@@ -93,7 +95,14 @@ var app = new Vue (
                 },
             ]
         },
+
+        methods: {
+
+            selectedContact(index) {
+                this.currentActiveElement = index;
+            }
+
+        }
     }
 
 );
-
